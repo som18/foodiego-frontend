1c88497fc6199c6b3704e7a3631341282b99540c
som18 <93382740+som18@users.noreply.github.com>
2024-06-01 20:21:26 +0530

    add user profile form feature

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -4,14 +4,16 @@ import HomePage from "./pages/HomePage";
 import AuthCallbackPage from "./pages/AuthCallbackPage";
 import UserProfilePage from "./pages/UserProfilePage";
 import ProtectedRoute from "./auth/ProtectedRoute";
+import ManageRestaurantPage from "./pages/ManageRestaurantPage";
 
 const AppRoutes = ()=>{
     return(
         <Routes>
-            <Route path="/" element={<Layout><HomePage showHero></HomePage></Layout>}/>
+            <Route path="/" element={<Layout><HomePage></HomePage></Layout>}/>
             <Route path="/auth-callback" element={<AuthCallbackPage/>}/>
             <Route element={<ProtectedRoute/>}>
             <Route path="/user-profile" element={<Layout><UserProfilePage/></Layout>}/>
+            <Route path="/manage-restaurant" element={<Layout><ManageRestaurantPage/></Layout>}/>
             </Route>
             
             <Route path="*" element={<Navigate to="/"/>}/>
